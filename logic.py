50bbd602303059040062e92e46d60aa8442862b5
Joseph Reed <48977926+josephereed@users.noreply.github.com>
2021-03-14 15:24:11 -0700

    introduce simple recursion
    added recursion in search algorithm, labels

--- logic.py
+++ logic.py
@@ -17,7 +17,26 @@ def search(x_y):
         new_y = (y + y_positions[i]) % 16
         if minefield[new_y][new_x] == 'B':
             sum += 1
-    print(sum)
+        else:
+            searchOnce(window.buttons[(new_x, new_y)], (new_x, new_y))
+    return sum
+
+def searchOnce(button, x_y):
+    sum = 0
+    x, y = x_y
+    for i in range(8):
+        x_positions = [11, 11, 11, 0, 13, 13, 13, 0]
+        y_positions = [15, 0, 17, 17, 17, 0, 15, 15] 
+        new_x = (x + x_positions[i]) % 12
+        new_y = (y + y_positions[i]) % 16
+        if minefield[new_y][new_x] == 'B':
+            sum += 1
+    if sum == 0:
+        button.hide()
+        searchOnce(window.buttons[(new_x, new_y)], (new_x, new_y))
+    button.setText(str(sum))
+    button.setStyleSheet('QPushButton {color: #FF0000}')
+
 
 
 def step(x_y, hide):
@@ -25,9 +44,10 @@ def step(x_y, hide):
     if minefield[y][x] == 'B':
         value = minefield[y][x]
         print("BOOM GAME OVER", f"({x}, {y}) minefield[y][x] = {value}")
-    else:
+    elif search(x_y) > 0:
+        print(search(x_y))
+    else: 
         hide()
-        search(x_y)
         print("safe", f"({x}, {y})")
 
 
--- window.py
+++ window.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
+buttons = {}
 
 class App(QWidget):
     def __init__(self):
@@ -18,19 +19,26 @@ class App(QWidget):
         self.initUI()
 
     def setButtons(self):
-        y = 0
-        for i in range(12 * 16):
-            if i == 12:
-                y = 1
-            x = i % 12
-            if i % 12 == 0 and i > 13:
-                y += 1
-            name = logic.minefield[y][x]
-            #name = f"({x},{y})"
-            self.button = QPushButton(f"{name}", self)
-            self.button.setGeometry(40, 40, 40, 40)
-            self.button.move((i % 12) * 40, y * 40)
-            self.button.clicked.connect(partial(self.on_click, (i % 12, y), self.button.hide))
+        # y = 0
+        # for i in range(12 * 16):
+        #     if i == 12:
+        #         y = 1
+        #     x = i % 12
+        #     if i % 12 == 0 and i > 13:
+        #         y += 1
+        #     name = logic.minefield[y][x]
+        #     name = f"({x},{y})"
+        #     self.button = QPushButton(f"{name}", self)
+        #     self.button.setGeometry(40, 40, 40, 40)
+        #     self.button.move((i % 12) * 40, y * 40)
+        #     self.button.clicked.connect(partial(self.on_click, (i % 12, y), self.button.hide))
+        for y in range(16):
+            for x in range(12):
+                name = logic.minefield[y][x]
+                buttons[(x, y)] = QPushButton(f'{name}', self)
+                buttons[(x, y)].setGeometry(40, 40, 40, 40)
+                buttons[(x,y)].move(x * 40, y * 40)
+                buttons[(x, y)].clicked.connect(partial(self.on_click, (x, y), buttons[(x, y)].hide))
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -40,4 +48,4 @@ class App(QWidget):
 
     def on_click(self, i, hide):
         logic.step(i, hide)
-        self.button.hide()
+        hide()
