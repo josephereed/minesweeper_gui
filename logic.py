0d529dcb93a23e008bca8c6d4682e5bc832ee367
Joseph Reed <48977926+josephereed@users.noreply.github.com>
2021-03-14 18:07:04 -0700

    Minimum Viable Product
    Full playthrough possible

--- logic.py
+++ logic.py
@@ -3,51 +3,56 @@ import sys
 import random
 
 minefield = [
-    ['B' if random.randint(0, 7) == 1 else ' ' for col in range(0, 12)]
+    ["B" if random.randint(0, 7) == 1 else " " for col in range(0, 12)]
     for row in range(0, 16)
 ]
+
 
 def search(x_y):
     sum = 0
     x, y = x_y
     for i in range(8):
         x_positions = [11, 11, 11, 0, 13, 13, 13, 0]
-        y_positions = [15, 0, 17, 17, 17, 0, 15, 15] 
+        y_positions = [15, 0, 17, 17, 17, 0, 15, 15]
         new_x = (x + x_positions[i]) % 12
         new_y = (y + y_positions[i]) % 16
-        if minefield[new_y][new_x] == 'B':
+        if minefield[new_y][new_x] == "B":
             sum += 1
         else:
             searchOnce(window.buttons[(new_x, new_y)], (new_x, new_y))
     return sum
+
 
 def searchOnce(button, x_y):
     sum = 0
     x, y = x_y
     for i in range(8):
         x_positions = [11, 11, 11, 0, 13, 13, 13, 0]
-        y_positions = [15, 0, 17, 17, 17, 0, 15, 15] 
+        y_positions = [15, 0, 17, 17, 17, 0, 15, 15]
         new_x = (x + x_positions[i]) % 12
         new_y = (y + y_positions[i]) % 16
-        if minefield[new_y][new_x] == 'B':
+        if minefield[new_y][new_x] == "B":
             sum += 1
     if sum == 0:
-        button.hide()
+        button.setEnabled(False)
         searchOnce(window.buttons[(new_x, new_y)], (new_x, new_y))
-    button.setText(str(sum))
-    button.setStyleSheet('QPushButton {color: #FF0000}')
+    button.setEnabled(False)
+    if sum > 0:
+        button.setText(str(sum))
 
 
-
-def step(x_y, hide):
+def step(x_y, button):
     x, y = x_y
-    if minefield[y][x] == 'B':
+    if minefield[y][x] == "B":
         value = minefield[y][x]
+        window.buttons[(x, y)].setStyleSheet(
+            " QPushButton {background-color: #DC143C }"
+        )
         print("BOOM GAME OVER", f"({x}, {y}) minefield[y][x] = {value}")
     elif search(x_y) > 0:
         print(search(x_y))
-    else: 
-        hide()
+    else:
+        button.setEnabled(False)
         print("safe", f"({x}, {y})")
 
 
--- window.py
+++ window.py
@@ -8,6 +8,7 @@ from PyQt5.QtCore import pyqtSlot
 
 buttons = {}
 
+
 class App(QWidget):
     def __init__(self):
         super().__init__()
@@ -19,33 +20,23 @@ class App(QWidget):
         self.initUI()
 
     def setButtons(self):
-        # y = 0
-        # for i in range(12 * 16):
-        #     if i == 12:
-        #         y = 1
-        #     x = i % 12
-        #     if i % 12 == 0 and i > 13:
-        #         y += 1
-        #     name = logic.minefield[y][x]
-        #     name = f"({x},{y})"
-        #     self.button = QPushButton(f"{name}", self)
-        #     self.button.setGeometry(40, 40, 40, 40)
-        #     self.button.move((i % 12) * 40, y * 40)
-        #     self.button.clicked.connect(partial(self.on_click, (i % 12, y), self.button.hide))
         for y in range(16):
             for x in range(12):
                 name = logic.minefield[y][x]
-                buttons[(x, y)] = QPushButton(f'{name}', self)
+                buttons[(x, y)] = QPushButton(f" ", self)
                 buttons[(x, y)].setGeometry(40, 40, 40, 40)
-                buttons[(x,y)].move(x * 40, y * 40)
-                buttons[(x, y)].clicked.connect(partial(self.on_click, (x, y), buttons[(x, y)].hide))
+                buttons[(x, y)].move(x * 40, y * 40)
+                buttons[(x, y)].clicked.connect(
+                    partial(self.on_click, (x, y), buttons[(x, y)])
+                )
 
     def initUI(self):
         self.setWindowTitle(self.title)
+        self.setWindowIcon(QIcon('icon.png'))
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setButtons()
         self.show()
 
-    def on_click(self, i, hide):
-        logic.step(i, hide)
-        hide()
+    def on_click(self, i, button):
+        logic.step(i, button)
+        button.setEnabled(False)
